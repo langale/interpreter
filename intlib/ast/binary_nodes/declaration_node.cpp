206a1057beb4574aa07b40f475f7310a099b20c5
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2026-04-16 20:30:16 +0200

    [intlib][ast] Evaluate comma-separated groups in the right hand side of the declaration

--- intlib/ast/binary_nodes/declaration_node.cpp
+++ intlib/ast/binary_nodes/declaration_node.cpp
@@ -34,10 +34,12 @@
 #if defined DEBUG
 #include <cassert>
 #endif
-
+#include <generator>
 #include <optional>
+#include <string>
 #include <ranges>
 #include <any>
+using namespace std::string_literals;
 
 #include <ale/ast/binary_nodes/SequenceNode.hpp>
 #include <ale/ast/binary_nodes/DeclarationNode.hpp>
@@ -59,22 +61,72 @@ namespace ast {
 
 #define aleprln ale::logger::println
 
+using GeneratorComma = std::generator<EvaluationResult>;
+using SharedGeneratorComma = std::shared_ptr<GeneratorComma>;
+
+[[nodiscard]] static GeneratorComma
+generate_empty(EvaluationContext&, const ale::ast::CommaSeparatedGroupNode&)
+{
+	co_return;
+}
+
+[[nodiscard]] static GeneratorComma generate_comma_values(
+	EvaluationContext& ctx, const ale::ast::CommaSeparatedGroupNode& comma
+)
+{
+	const auto& children = comma.get_children();
+	for (const auto& child : children) {
+
+		EvaluationResult res_w = interpret_node(ctx, child);
+		if (not res_w.has_value()) {
+			INTERPRETER_PRINT_LOC(aleprln, "Evaluation of node failed.");
+			co_yield make_bad_evaluation_result(std::move(res_w.error()));
+			co_return;
+		}
+
+		std::any value_w = std::move(*res_w);
+
+		INTERPRETER_PRINT_LOC(
+			aleprln,
+			"Type returned from node evaluation is: '{}'. Value is: '{}'.",
+			detail::get_type_name(value_w),
+			any_view{value_w}
+		);
+
+		co_yield make_good_evaluation_result<std::any>(std::move(value_w));
+	}
+}
+
 [[nodiscard]] static EvaluationResult compute_value_from_declaration(
 	EvaluationContext& ctx, const ale::ast::DeclarationNode& decl
 )
 {
 	INTERPRETER_ENTER_AST_FUNCTION(aleprln);
 
-	const auto& value_node = decl.get_right_child();
 	std::any value_w;
 	if (decl.get_node_type() != ale::ast::node_type_e::Declaration_Declare) {
+
+		const auto& value_node = decl.get_right_child();
+
+		if (value_node->get_node_type() ==
+			ale::ast::node_type_e::Comma_Separated_Group) {
+
+			const ale::ast::CommaSeparatedGroupNode& comma =
+				*static_cast<ale::ast::CommaSeparatedGroupNode *>(
+					value_node.get()
+				);
+
+			return make_good_evaluation_result<SharedGeneratorComma>(
+				std::make_shared<GeneratorComma>(
+					generate_comma_values(ctx, comma)
+				)
+			);
+		}
+
 		EvaluationResult res_w = interpret_node(ctx, value_node);
 		if (not res_w.has_value()) {
 			INTERPRETER_PRINT_LOC(aleprln, "Evaluation of node failed.");
-			return make_bad_evaluation_result(
-				std::vector{evaluation_error_e::Evaluation_Of_Node_Failed},
-				std::vector<std::string>{"Evaluation of node failed."}
-			);
+			return make_bad_evaluation_result(std::move(res_w.error()));
 		}
 		value_w = std::move(*res_w);
 
@@ -194,11 +246,7 @@ namespace ast {
 		INTERPRETER_PRINT_LOC(
 			aleprln, "Could not make the name of the variable."
 		);
-		return make_bad_evaluation_result(
-			std::vector{evaluation_error_e::Evaluation_Of_Node_Failed},
-			std::vector<std::string>{"Evaluation of node failed and the name "
-									 "of the variable could not be made."}
-		);
+		return make_bad_evaluation_result(std::move(res_w.error()));
 	}
 
 	const std::any& name_w = *res_w;
@@ -216,11 +264,13 @@ namespace ast {
 	);
 }
 
+template <bool value_is_group, typename generator_iterator_t>
 [[nodiscard]] static EvaluationResult declare_variable_sequence(
 	EvaluationContext& ctx,
 	const ale::ast::DeclarationNode& decl,
 	const std::unique_ptr<ale::ast::Node>& sequence,
-	const std::any& value_w
+	const std::any& value_w,
+	generator_iterator_t& comma_iterator
 )
 {
 	INTERPRETER_ENTER_AST_FUNCTION(aleprln);
@@ -229,13 +279,7 @@ namespace ast {
 
 	auto res_w = make_shallow_sequence_indices(ctx, sequence);
 	if (not res_w) {
-		return make_bad_evaluation_result(
-			std::vector{evaluation_error_e::Evaluation_Of_Node_Failed},
-			std::vector<std::string>{
-				"Evaluation of node failed and the name of the variables in a "
-				"sequence could not be made."
-			}
-		);
+		return make_bad_evaluation_result(std::move(res_w.error()));
 	}
 
 	INTERPRETER_PRINT_LOC(aleprln, "Successfully made list of names.");
@@ -261,22 +305,47 @@ namespace ast {
 
 		std::string var_name = base_name;
 		append_variable_name(var_name, indices);
+#if defined ALE_LOGGING_MESSAGES
+		const std::string var_name_copy = var_name;
+#endif
 
 		INTERPRETER_PRINT_LOC(aleprln, "Variable name: {}.", var_name);
 
 		std::string var_type = decl.get_variable_type();
-		auto declare_res_w = declare_single_variable(
-			ctx,
-			decl.get_node_type(),
-			std::move(var_name),
-			std::move(var_type),
-			value_w
-		);
+		EvaluationResult declare_res_w;
+
+		if constexpr (value_is_group) {
+			/// TODO: figure out why removing "const &" is a compilation error
+			const EvaluationResult& group_value_res_w = *comma_iterator;
+			if (not group_value_res_w) {
+				return make_bad_evaluation_result(group_value_res_w.error());
+			}
+
+			const std::any& group_value_w = *group_value_res_w;
+
+			declare_res_w = declare_single_variable(
+				ctx,
+				decl.get_node_type(),
+				std::move(var_name),
+				std::move(var_type),
+				group_value_w
+			);
+			++comma_iterator;
+		}
+		else {
+			declare_res_w = declare_single_variable(
+				ctx,
+				decl.get_node_type(),
+				std::move(var_name),
+				std::move(var_type),
+				value_w
+			);
+		}
 
 		INTERPRETER_PRINT_LOC(
 			aleprln,
 			"    Successfully assigned variable with name '{}'.",
-			var_name
+			var_name_copy
 		);
 
 		if (not declare_res_w) {
@@ -297,6 +366,8 @@ namespace ast {
 {
 	INTERPRETER_ENTER_AST_FUNCTION(aleprln);
 
+	const bool is_declare =
+		decl.get_node_type() == ale::ast::node_type_e::Declaration_Declare;
 	const auto& variable_list_node_w = decl.get_left_child();
 
 #if defined DEBUG
@@ -305,12 +376,37 @@ namespace ast {
 	);
 #endif
 
-	EvaluationResult res_compute_w = compute_value_from_declaration(ctx, decl);
-	if (not res_compute_w) {
-		return std::move(res_compute_w.error());
+	auto compute_res_w = compute_value_from_declaration(ctx, decl);
+	if (not compute_res_w) {
+		return std::move(compute_res_w.error());
 	}
-	const std::any value_w = std::move(*res_compute_w);
-	return declare_variable_sequence(ctx, decl, sequence, value_w);
+	const std::any value_w = std::move(*compute_res_w);
+
+	const bool value_is_group =
+		not is_declare and decl.get_right_child()->get_node_type() ==
+							   ale::ast::node_type_e::Comma_Separated_Group;
+
+	[[maybe_unused]] SharedGeneratorComma comma_values_w;
+	ale::ast::CommaSeparatedGroupNode placeholder_node;
+	if (value_is_group) {
+		comma_values_w = std::any_cast<SharedGeneratorComma>(value_w);
+	}
+	else {
+		comma_values_w = std::make_shared<GeneratorComma>(
+			generate_empty(ctx, placeholder_node)
+		);
+	}
+	[[maybe_unused]] auto pos = comma_values_w->begin();
+	[[maybe_unused]] auto end = comma_values_w->end();
+
+	if (not is_declare and decl.get_right_child()->get_node_type() ==
+							   ale::ast::node_type_e::Comma_Separated_Group) {
+
+		return declare_variable_sequence<true>(
+			ctx, decl, sequence, value_w, pos
+		);
+	}
+	return declare_variable_sequence<false>(ctx, decl, sequence, value_w, pos);
 }
 
 [[nodiscard]] static EvaluationResult declare_comma_separated_variables(
@@ -319,6 +415,8 @@ namespace ast {
 {
 	INTERPRETER_ENTER_AST_FUNCTION(aleprln);
 
+	const bool is_declare =
+		decl.get_node_type() == ale::ast::node_type_e::Declaration_Declare;
 	const auto& variable_list_node_w = decl.get_left_child();
 
 #if defined DEBUG
@@ -328,28 +426,75 @@ namespace ast {
 	);
 #endif
 
-	EvaluationResult res_compute_w = compute_value_from_declaration(ctx, decl);
-	if (not res_compute_w) {
-		return std::move(res_compute_w.error());
+	auto compute_res_w = compute_value_from_declaration(ctx, decl);
+	if (not compute_res_w) {
+		return std::move(compute_res_w.error());
 	}
-	const std::any value_w = std::move(*res_compute_w);
+	const std::any value_w = std::move(*compute_res_w);
+
+	const bool value_is_group =
+		not is_declare and decl.get_right_child()->get_node_type() ==
+							   ale::ast::node_type_e::Comma_Separated_Group;
+
+	[[maybe_unused]] SharedGeneratorComma comma_values_w;
+	ale::ast::CommaSeparatedGroupNode placeholder_node;
+	if (value_is_group) {
+		comma_values_w = std::any_cast<SharedGeneratorComma>(value_w);
+	}
+	else {
+		comma_values_w = std::make_shared<GeneratorComma>(
+			generate_empty(ctx, placeholder_node)
+		);
+	}
+	[[maybe_unused]] auto pos = comma_values_w->begin();
+	[[maybe_unused]] auto end = comma_values_w->end();
 
 	const auto& variable_list_node =
 		*static_cast<const ale::ast::CommaSeparatedGroupNode *>(
 			variable_list_node_w.get()
 		);
 
+	[[maybe_unused]] std::any group_value_w;
 	const auto& children = variable_list_node.get_children();
-
 	for (const auto& child : children) {
+
 		if (child->get_node_type() == ale::ast::node_type_e::Variable) {
-			auto res_w = declare_variable(ctx, decl, child, value_w);
+
+			EvaluationResult res_w;
+			if (value_is_group) {
+				/// TODO: figure out why removing "const &" is a compilation error
+				const EvaluationResult& group_value_res_w = *pos;
+				if (not group_value_res_w) {
+					return make_bad_evaluation_result(
+						group_value_res_w.error()
+					);
+				}
+
+				group_value_w = *group_value_res_w;
+				++pos;
+
+				res_w = declare_variable(ctx, decl, child, group_value_w);
+			}
+			else {
+				res_w = declare_variable(ctx, decl, child, value_w);
+			}
 			if (not res_w) {
 				return make_bad_evaluation_result(std::move(res_w.error()));
 			}
 		}
 		else if (child->get_node_type() == ale::ast::node_type_e::Sequence) {
-			auto res_w = declare_variable_sequence(ctx, decl, child, value_w);
+
+			EvaluationResult res_w;
+			if (value_is_group) {
+				res_w = declare_variable_sequence<true>(
+					ctx, decl, child, value_w, pos
+				);
+			}
+			else {
+				res_w = declare_variable_sequence<false>(
+					ctx, decl, child, value_w, pos
+				);
+			}
 			if (not res_w) {
 				return make_bad_evaluation_result(std::move(res_w.error()));
 			}
@@ -357,8 +502,26 @@ namespace ast {
 		else if (child->get_node_type() ==
 				 ale::ast::node_type_e::Subscripted_Variable) {
 
-			auto res_w =
-				declare_subscripted_variable(ctx, decl, child, value_w);
+			EvaluationResult res_w;
+			if (value_is_group) {
+				/// TODO: figure out why removing "const &" is a compilation error
+				const EvaluationResult& group_value_res_w = *pos;
+				if (not group_value_res_w) {
+					return make_bad_evaluation_result(
+						group_value_res_w.error()
+					);
+				}
+
+				group_value_w = *group_value_res_w;
+				++pos;
+
+				res_w = declare_subscripted_variable(
+					ctx, decl, child, group_value_w
+				);
+			}
+			else {
+				res_w = declare_subscripted_variable(ctx, decl, child, value_w);
+			}
 			if (not res_w) {
 				return make_bad_evaluation_result(std::move(res_w.error()));
 			}
