4a0c348c6d0537f66a461a5d2f4482e41ea27a1c
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2026-04-27 16:21:00 +0200

    Minor changes

--- apply_style.sh
+++ apply_style.sh
@@ -2,6 +2,6 @@
 
 for d in intlib interpreter; do
 	cd $d
-	find . -iname '*.hpp' -o -iname '*.cpp' | xargs /usr/bin/clang-format-19 -i
+	find . -iname '*.hpp' -o -iname '*.cpp' | xargs /usr/bin/clang-format-19 -style=file -i
 	cd ..
 done
