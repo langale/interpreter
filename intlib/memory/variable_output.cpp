f5ebfba7e269350af0082904b17e507b43afd0c2
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2026-04-30 18:38:18 +0200

    [intlib][memory] Output variable contents through its own formatter

---
+++ intlib/memory/variable_output.cpp
@@ -0,0 +1,49 @@
+/*********************************************************************
+ *
+ * ALE language -- an interpreted programming language
+ * Copyright (C) 2024 - 2026 Lluís Alemany Puig
+ *
+ * This file is part of the implementation of ALE. The full code is
+ * available at:
+ *
+ *     https://github.com/langale/ale
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Contact:
+ *
+ *     Lluís Alemany Puig
+ *     lluis.alemany.puig@gmail.com
+ *     https://github.com/lluisalemanypuig
+ *
+ ********************************************************************/
+
+#include <format>
+
+#include <intlib/detail/any_output.hpp>
+#include <intlib/memory/VariableValue.hpp>
+#include <intlib/memory/variable_output.hpp>
+
+std::formatter<intlib::memory::VariableValue>::OutT
+std::formatter<intlib::memory::VariableValue>::format(
+	const intlib::memory::VariableValue& var, std::format_context& ctx
+) const
+{
+	return std::format_to(
+		ctx.out(),
+		"Variable: {} = {}",
+		var.type,
+		intlib::detail::AnyView{var.value_w}
+	);
+}
