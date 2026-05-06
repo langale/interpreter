3e2d49ff3ed9bad734cf6a6e4d9873208972a675
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2026-05-06 17:52:22 +0200

    [intlib][memory] Added unwrap idiom for wrapped variables

---
+++ intlib/memory/utils/unwrap.cpp
@@ -0,0 +1,81 @@
+/*********************************************************************
+ *
+ * ALE interpreter library -- the base utilities for a command line utility
+ * to run programs written in ALE
+ *
+ *     Copyright (C) 2024 - 2026 Lluís Alemany Puig
+ *
+ * This file is part of the implementation of an interpreter for ALE.
+ * The full code is available at:
+ *
+ *     https://github.com/langale/interpreter
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Contact:
+ *
+ *     Lluís Alemany Puig
+ *     lluis.alemany.puig@gmail.com
+ *     https://github.com/lluisalemanypuig
+ *
+ ********************************************************************/
+
+#include <intlib/memory/WrappedAny.hpp>
+#include <intlib/memory/Variable.hpp>
+
+namespace intlib {
+namespace memory {
+
+const WrappedAny *unwrap_out(const WrappedAny& w)
+{
+	const WrappedAny *actual_w = nullptr;
+	if (w.type == detail::type_string_cpp<RefVar>) {
+		actual_w = &std::any_cast<RefVar>(w.value).get().wrap;
+	}
+	else if (w.type == detail::type_string_cpp<RefConstVar>) {
+		actual_w = &std::any_cast<const RefVar>(w.value).get().wrap;
+	}
+	else {
+		actual_w = &w;
+	}
+	return actual_w;
+}
+
+WrappedAny *unwrap_out(WrappedAny& w)
+{
+	WrappedAny *actual_w = nullptr;
+	if (w.type == detail::type_string_cpp<RefVar>) {
+		actual_w = &std::any_cast<RefVar>(w.value).get().wrap;
+	}
+	else if (w.type == detail::type_string_cpp<RefConstVar>) {
+		actual_w = &std::any_cast<const RefVar>(w.value).get().wrap;
+	}
+	else {
+		actual_w = &w;
+	}
+	return actual_w;
+}
+
+void unwrap_into(WrappedAny& w)
+{
+	if (w.type == detail::type_string_cpp<RefVar>) {
+		w = std::any_cast<RefVar>(w.value).get().wrap;
+	}
+	else if (w.type == detail::type_string_cpp<RefConstVar>) {
+		w = std::any_cast<const RefVar>(w.value).get().wrap;
+	}
+}
+
+} // namespace memory
+} // namespace intlib
